8723dfbc0ceb32b5fa2d66cea81a67784c36d2c2
Pawan kumar <pawankumarpawankumar619@gmail.com>
2020-05-09 04:25:48 +0530

    Added First Folder of my programs

---
+++ JavaAid/SumOfRange.java
@@ -0,0 +1,25 @@
+package JavaAid;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumOfRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int []arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = sumOfRange(arr,2,6);
+        System.out.println(sum);
+    }
+
+    public static int sumOfRange(int []arr,int i,int j){
+        for (int k = 1; k < arr.length; k++) {
+            arr[k] = arr[k-1] + arr[k];
+        }
+        System.out.println(Arrays.toString(arr));
+        return arr[j] - arr[i-1];
+    }
+}
