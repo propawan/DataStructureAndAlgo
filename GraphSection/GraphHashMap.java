263f40ebdbe803541b3613bad795344dffa13d08
Pawan kumar <pawankumarpawankumar619@gmail.com>
2020-05-09 15:12:32 +0530

    Added Graph Section

---
+++ GraphSection/GraphHashMap.java
@@ -0,0 +1,252 @@
+package GraphSection;
+
+import java.util.*;
+
+public class GraphHashMap<T> {
+
+    HashMap<T, Vertex> vertexMap;
+
+    public GraphHashMap() {
+        this.vertexMap = new HashMap<>();
+    }
+
+    public class Vertex {
+        T value;
+        HashMap<Vertex, Integer> neighbours;
+
+        public Vertex(T value) {
+            this.value = value;
+            this.neighbours = new HashMap<>();
+        }
+    }
+
+    public void addVertex(T value) {
+        if (!vertexMap.containsKey(value)) {
+            vertexMap.put(value, new Vertex(value));
+        }
+    }
+
+    public void addEdge(T x, T y, boolean isBidirectional, int weight) {
+        if (vertexMap.containsKey(x) && vertexMap.containsKey(y)) {
+            vertexMap.get(x).neighbours.put(vertexMap.get(y), weight);
+        }
+        if (isBidirectional) {
+            vertexMap.get(y).neighbours.put(vertexMap.get(x), weight);
+        }
+    }
+
+    public void display() {
+        for (T val : vertexMap.keySet()) {
+            System.out.print(val + "-> ");
+            for (Vertex neighbour : vertexMap.get(val).neighbours.keySet()) {
+                System.out.print(neighbour.value + " " + vertexMap.get(val).neighbours.get(neighbour) + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void bft(T source) {
+        Vertex vertex = vertexMap.get(source);
+        Set<Vertex> set = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+        queue.add(vertex);
+        set.add(vertex);
+        while (!queue.isEmpty()) {
+            Vertex curr = queue.remove();
+            System.out.print(curr.value + " ");
+            for (Vertex neighbour : curr.neighbours.keySet()) {
+                if (!set.contains(neighbour)) {
+                    set.add(neighbour);
+                    queue.add(neighbour);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public int sSsp(T source, T dest) {
+        Vertex vertex = vertexMap.get(source);
+        HashMap<T, Integer> map = new HashMap<>();
+        Queue<Vertex> q = new LinkedList<>();
+        q.add(vertex);
+
+        HashMap<T, T> parent = new HashMap<>();
+
+        for (T val : vertexMap.keySet()) {
+            map.put(val, Integer.MAX_VALUE);
+        }
+        map.put(source, 0);
+        parent.put(source, source);
+
+        while (!q.isEmpty()) {
+            Vertex curr = q.remove();
+            for (Vertex neighbour : curr.neighbours.keySet()) {
+                if (map.get(neighbour.value) == Integer.MAX_VALUE) {
+                    q.add(neighbour);
+                    parent.put(neighbour.value, curr.value);
+                    map.put(neighbour.value, map.get(curr.value) + 1);
+                }
+            }
+        }
+
+//        for (T val:vertexMap.keySet()) {
+//            System.out.println("Vertex: "+source+" Distance from: "+val+" is "+map.get(val));
+//        }
+
+        T temp = dest;
+
+        while (temp != source) {
+            System.out.print(temp + "<-");
+            temp = parent.get(temp);
+        }
+        System.out.println(source);
+        return map.get(dest);
+    }
+
+    public void dfs(T source) {
+        Set<T> visited = new HashSet<>();
+        dfsHelper(source, visited);
+    }
+
+    public void dfsHelper(T source, Set<T> visited) {
+        System.out.print(source + " ");
+        visited.add(source);
+        for (Vertex vertex : vertexMap.get(source).neighbours.keySet()) {
+            if (!visited.contains(vertex.value)) {
+                dfsHelper(vertex.value, visited);
+            }
+        }
+    }
+
+    public void connectedComp() {
+        Set<T> visited = new HashSet<>();
+        int count = 0;
+        System.out.println();
+        for (T val : vertexMap.keySet()) {
+            if (!visited.contains(val)) {
+                System.out.print("Component " + count + " ");
+                dfsHelper(val, visited);
+                count++;
+                System.out.println();
+            }
+        }
+    }
+
+    public void topSort() {
+        Set<T> visited = new HashSet<>();
+        LinkedList<T> list = new LinkedList<>();
+
+        for (T val : vertexMap.keySet()) {
+            if (!visited.contains(val)) {
+                topSortHelper(val, visited, list);
+            }
+        }
+
+        for (T val : list) {
+            System.out.println(val);
+        }
+    }
+
+    public void topSortHelper(T source, Set<T> visited, LinkedList<T> list) {
+        visited.add(source);
+
+        for (Vertex vertex : vertexMap.get(source).neighbours.keySet()) {
+            if (!visited.contains(vertex.value)) {
+                topSortHelper(vertex.value, visited, list);
+            }
+        }
+        list.addFirst(source);
+    }
+
+    public void topSortBfs() {
+        HashMap<T, Integer> inDegree = new HashMap<>();
+        Queue<T> queue = new LinkedList<>();
+
+        for (T val : vertexMap.keySet()) {
+            inDegree.put(val, 0);
+        }
+
+        for (T val : vertexMap.keySet()) {
+            for (Vertex vertex : vertexMap.get(val).neighbours.keySet()) {
+                inDegree.put(vertex.value, inDegree.get(vertex.value) + 1);
+            }
+        }
+
+        for (T val : vertexMap.keySet()) {
+            if (inDegree.get(val) == 0) {
+                queue.add(val);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            T curr = queue.remove();
+            System.out.println(curr);
+            for (Vertex vertex : vertexMap.get(curr).neighbours.keySet()) {
+                inDegree.put(vertex.value, inDegree.get(vertex.value) - 1);
+                if (inDegree.get(vertex.value) == 0) {
+                    queue.add(vertex.value);
+                }
+            }
+        }
+
+    }
+
+    public boolean isTree() {
+        Set<T> visited = new HashSet<>();
+        Queue<T> queue = new LinkedList<>();
+        Map<T, T> parent = new HashMap<>();
+
+        visited.add(vertexMap.get(0).value);
+        queue.add(vertexMap.get(0).value);
+
+        for (T val : vertexMap.keySet()) {
+            parent.put(val, val);
+        }
+
+        while (!queue.isEmpty()) {
+            T val = queue.remove();
+            for (Vertex vertex : vertexMap.get(val).neighbours.keySet()) {
+                if (visited.contains(vertex.value) && parent.get(val) != vertex.value) {
+                    return false;
+                } else if (!visited.contains(vertex.value)) {
+                    queue.add(vertex.value);
+                    visited.add(vertex.value);
+                    parent.put(vertex.value, val);
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean hasCycle() {
+        Set<T> visited = new HashSet<>();
+        for (T val : vertexMap.keySet()) {
+            if (!visited.contains(val)) {
+                boolean flag = hasCycleHelper(val, visited, val);
+                if (flag) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean hasCycleHelper(T val,Set<T> visited,T parent){
+        visited.add(val);
+        Vertex vertex = vertexMap.get(val);
+
+        for (Vertex neighbour: vertex.neighbours.keySet()) {
+            if(!visited.contains(neighbour.value)){
+                boolean has_cycle = hasCycleHelper(neighbour.value,visited,val);
+                if(has_cycle){
+                    return true;
+                }
+            }
+            else if(!parent.equals(val)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
---
+++ GraphSection/GraphHashMapClient.java
@@ -0,0 +1,104 @@
+package GraphSection;
+
+import JustForRevision.GraphHashMap;
+
+public class GraphHashMapClient {
+    public static void main(String[] args) {
+        GraphHashMap<Integer> graph = new GraphHashMap<>();
+//        Topological sort BFS
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addEdge(0,2,false,1);
+        graph.addEdge(1,2,false,1);
+        graph.addEdge(1,4,false,1);
+        graph.addEdge(2,5,false,1);
+        graph.addEdge(2,3,false,1);
+        graph.addEdge(4,5,false,1);
+        graph.addEdge(3,5,false,1);
+//        graph.topSortBfs();
+//        System.out.println(graph.isTree());
+
+//        Topological Sort Example that what should be learnt first
+//        graph.addVertex("Python");
+//        graph.addVertex("Data Pre-processing");
+//        graph.addVertex("PyTorch");
+//        graph.addVertex("Ml Basics");
+//        graph.addVertex("Deep Learning");
+//        graph.addVertex("Face Recognition");
+//        graph.addVertex("Dataset");
+//        graph.addEdge("Python","Data Pre-processing",false,10);
+//        graph.addEdge("Python","Ml Basics",false,10);
+//        graph.addEdge("Python","PyTorch",false,10);
+//        graph.addEdge("PyTorch","Deep Learning",false,10);
+//        graph.addEdge("Ml Basics","Deep Learning",false,10);
+//        graph.addEdge("Deep Learning","Face Recognition",false,10);
+//        graph.addEdge("Dataset","Face Recognition",false,10);
+//        graph.topSort();
+
+//        Has Cycle check
+//        (Add a Vertex of 6 if not present)
+//        graph.addEdge(0,1,false,1);
+//        graph.addEdge(2,3,false,1);
+//        graph.addEdge(1,3,false,1);
+//        graph.addEdge(0,2,false,1);
+//        graph.addEdge(4,5,false,1);
+//        graph.addEdge(5,6,false,1);
+//        graph.addEdge(6,4,false,1);
+//        System.out.println(graph.hasCycle());
+
+//        graph.addVertex('A');
+//        graph.addVertex('B');
+//        graph.addVertex('C');
+//        graph.addVertex('D');
+//        graph.addVertex('E');
+//        graph.addVertex('F');
+//        graph.addVertex('G');
+//        graph.addVertex('H');
+//        graph.addVertex('I');
+//        graph.addEdge('A','B',true,10);
+//        graph.addEdge('B','C',true,20);
+//        graph.addEdge('C','D',true,30);
+//        graph.addEdge('D','A',true,10);
+//        graph.addEdge('D','E',true,15);
+//        graph.addEdge('E','F',true,10);
+//        graph.addEdge('G','H',true,10);
+//        graph.display();
+//        graph.bft('B');
+//        System.out.println(graph.sSsp('A','E'));
+//        graph.dfs('A');
+//        graph.connectedComp();
+
+//        For Snake And Ladders
+
+//        int []board = new int[50];
+//        board[2] = 13;
+//        board[5] = 2;
+//        board[9] = 18;
+//        board[18] = 11;
+//        board[17] = -13;
+//        board[20] = -14;
+//        board[24] = -8;
+//        board[25] = 10;
+//        board[32] = -2;
+//        board[34] = -22;
+//
+//        for (int i = 0; i < 36; i++) {
+//            graph.addVertex(i);
+//            for (int j = 1; j <= 6; j++) {
+//                int k = i+j;
+//                k += board[k];
+//                if(k<=36){
+//                    graph.addVertex(k);
+//                    graph.addEdge(i,k,false,1);
+//                }
+//            }
+//        }
+//        graph.addVertex(36);
+//        graph.addEdge(36,36,false,1);
+//        System.out.println(graph.sSsp(0,36));
+    }
+}
