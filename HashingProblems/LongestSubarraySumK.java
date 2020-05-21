df444028a6bee5a1fbeacf372d384f646a1c5721
Pawan kumar <pawankumarpawankumar619@gmail.com>
2020-05-21 22:28:32 +0530

    Longest sub array with sum k added

---
+++ HashingProblems/LongestSubarraySumK.java
@@ -0,0 +1,36 @@
+package HashingProblems;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class LongestSubarraySumK {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(longestSubarrayWithSumK(arr,k));
+    }
+
+    public static int longestSubarrayWithSumK(int[] arr,int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int pre = 0;
+        int maxLength = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            pre += arr[i];
+            if(pre==k){
+                maxLength = Integer.max(maxLength,i+1);
+            }
+            if(map.containsKey(pre-k)){
+                maxLength = Integer.max(maxLength,i-map.get(pre-k));
+            }
+            else{
+                map.put(pre,i);
+            }
+        }
+        return maxLength;
+    }
+}
