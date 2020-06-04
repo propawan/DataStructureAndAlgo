43d44140f660911de02d55caf839ff90827114f2
Pawan kumar <pawankumarpawankumar619@gmail.com>
2020-06-04 21:14:25 +0530

    Added memoization in recursive approach of TotalBSTWithNNodes

--- DynamicProgramming/TotalBSTWithNNodes.java
+++ DynamicProgramming/TotalBSTWithNNodes.java
@@ -2,8 +2,10 @@ package DynamicProgramming;
 
 public class TotalBSTWithNNodes {
     public static void main(String[] args) {
+        int n = 4;
+        int[] mem = new int[n+1];
         System.out.println(totalBST(4));
-        System.out.println(totalBSTRec(4));
+        System.out.println(totalBSTRec(4,mem));
     }
 
     public static int totalBST(int N){
@@ -22,16 +24,21 @@ public class TotalBSTWithNNodes {
         return mem[N];
     }
 
-    public static int totalBSTRec(int n){
+    public static int totalBSTRec(int n,int[] mem){
 //        Top Down Approach
         if(n<=1){
             return 1;
         }
 
+        if(mem[n]!=0){
+            return mem[n];
+        }
+
         int ans = 0;
         for (int i = 1; i <= n; i++) {
-            ans += totalBSTRec(i-1)*totalBSTRec(n-i);
+            ans += totalBSTRec(i-1,mem)*totalBSTRec(n-i,mem);
         }
+        mem[n] = ans;
         return ans;
     }
 }
